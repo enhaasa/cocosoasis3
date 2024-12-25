b92c55f5d19e47dbc073125c3ceebe6d24100a79
Enhasa <enhaasa@pm.me>
2024-12-25 20:33:35 +0100

    Remove incorrect defence clause.

--- backend/update_discord_events.php
+++ backend/update_discord_events.php
@@ -3,16 +3,6 @@ require __DIR__ . '/env.php';
 require __DIR__ . '/cors.php';
 require_once __DIR__ . '/vendor/autoload.php';
 
-$postBody = file_get_contents('php://input');
-
-if (!$postBody) {
-    http_response_code(401);
-}
-
-
-
-$data = json_decode($postBody, true);
-
 $DISCORD_INFO_URL = $_ENV['DISCORD_INFO_URL'];
 
 $query = $DISCORD_INFO_URL . '?data=Events';
