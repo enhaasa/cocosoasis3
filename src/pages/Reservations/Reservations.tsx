52a35d68f2242b3cd2b768accf6f88e0cb1a9daa
Enhasa <enhaasa@pm.me>
2025-01-12 16:09:13 +0100

    Add deployment instructions for prod.

--- src/pages/Reservations/Reservations.tsx
+++ src/pages/Reservations/Reservations.tsx
@@ -108,7 +108,6 @@ export default function Reservations() {
                             </div>
                         }
                     </div>
-
                 </div>
             </div>
         </Page>    
