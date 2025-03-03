481f8223bed97e4e1deea539d9507553b6df37a9
Enhasa <enhaasa@pm.me>
2025-03-03 17:50:23 +0100

    Test seating plan concept.

--- src/components/SeatingPlanModal/SeatingPlan/SeatingPlan.tsx
+++ src/components/SeatingPlanModal/SeatingPlan/SeatingPlan.tsx
@@ -1,20 +1,73 @@
 import styles from './SeatingPlan.module.scss';
-//import { useContext } from 'react';
+import { useContext, useState, useRef, useEffect } from 'react';
 
 // Contexts
-//import { KiwiContext } from '@contexts/Kiwi';
+import { KiwiContext } from '@contexts/Kiwi';
 
-//const SECTION_HEIGHT = '700px';
-//const SECTION_WIDTH = '1000px';
+const ORIGINAL_HEIGHT = 700;
+const ORIGINAL_WIDTH = 1000;
+
+const SEATING_HEIGHT = 40;
+const SEATING_WIDTH = 40;
 
 export default function SeatingPlan() {
-    //const { seatings } = useContext(KiwiContext);
+    const { seatings } = useContext(KiwiContext);
+    const [selectedSection, setSelectedSection] = useState(seatings.getSections()[0]);
 
-    //console.log('seatings', seatings.data)
+    const [seatingOffset, setSeatingOffset] = useState({ scaleX: 1, scaleY: 1 });
+
+    const sectionRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (!sectionRef.current) return;
+
+        const observer = new ResizeObserver(entries => {
+            for (let entry of entries) {
+                const { width, height } = entry.contentRect;
+
+                const scaleX = width / ORIGINAL_WIDTH;
+                const scaleY = height / ORIGINAL_HEIGHT;
+
+                setSeatingOffset({ scaleX, scaleY });
+            }
+        });
+
+        observer.observe(sectionRef.current);
+
+        return () => observer.disconnect();
+    }, []);
 
     return (
         <div className={styles.container}>
-            
+            <nav>
+                {seatings.getSections().map(section => (
+                    <button key={section.id} onClick={() => setSelectedSection(section)}>
+                        {section.name}
+                    </button>
+                ))}
+            </nav>
+
+            <div className={styles.section} ref={sectionRef}>
+                <img 
+                    src={selectedSection.image_url.trim()} 
+                    alt={`Section ${selectedSection.name}`} 
+                />
+
+                {seatings.getSeatingsBySectionId(selectedSection.id).map(seating => (
+                    <div 
+                        key={seating.id}
+                        className={`${styles.seating} ${styles[seating.availability]}`}
+                        style={{
+                            left: (seating.pos_x * seatingOffset.scaleX),
+                            top: (seating.pos_y * seatingOffset.scaleY),
+                            height: SEATING_HEIGHT,
+                            width: SEATING_WIDTH
+                        }}
+                    >
+                        {seating.number}
+                    </div>                        
+                ))}
+            </div>
         </div>    
     );
 }
--- src/hooks/kiwi/useSeatings.ts
+++ src/hooks/kiwi/useSeatings.ts
@@ -2,22 +2,55 @@ import { useState, useEffect } from "react";
 import { KiwiClient } from "@service_clients/KiwiClient";
 
 // Types
-import { TSeating } from "@enhasa/kiwicore";
+import { type Seating } from "@service_clients/KiwiClient";
+
+export type Section = {
+    id: number;
+    image_url: string;
+    name: string;
+}
 
 export type UseSeatings = {
-    data: TSeating[] | null;
+    data: Seating[] | null;
+    getSections: () => Section[];
+    getSeatingsBySectionId: (id: number) => Seating[];
 };
 
+
 export default function useSeatings(client: KiwiClient) {
-    const [ data, setData ] = useState<null | TSeating[]>(null);
+    const [ data, setData ] = useState<null | Seating[]>(null);
 
     useEffect(() => {
         client.getSeatings().then(res => {
-            setData(res);
+            setData(res.filter(seating => seating.section_name !== 'Bar'));
         });
     }, []);
+
+    function getSections() {
+        const sections: Section[] = [];
+
+        data?.forEach(seating => {
+            let exists = sections.find(s => s.id === seating.section_id);
+
+            if (!exists) {
+                sections.push({
+                    id: seating.section_id,
+                    image_url: seating.image_url,
+                    name: seating.section_name
+                });
+            }
+        });
+
+        return sections;
+    }
+
+    function getSeatingsBySectionId(id: number) {
+        return data?.filter(seating => seating.section_id === id) ?? [];
+    }
  
     return {
-        data
+        data,
+        getSections,
+        getSeatingsBySectionId
     }
 }
