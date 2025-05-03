efca4237bb7cd23d387fea6190702e2efa971099
Enhasa <enhaasa@pm.me>
2025-05-03 13:19:42 +0200

    Switch from useState's to useReducer for modals.

--- src/hooks/modals/useModals.ts
+++ src/hooks/modals/useModals.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState, ReactElement } from 'react';
+import React, { ReactElement, useReducer } from 'react';
 
 // Constants
 import { DEFAULT_MOUNT_DELAY, DEFAULT_DISMOUNT_DELAY } from './modalDefaults';
@@ -11,11 +11,24 @@ export type TModal = {
     mountDelay: number;
     dismountDelay: number;
 }
+type TModals = Record<string, TModal>;
+
+type State = {
+    modals: TModals;
+    isChangingState: boolean;
+}
+
+type Action = 
+    | { type: 'ADD_MODAL'; modal: TModalComponent }
+    | { type: 'BEGIN_REMOVAL'; id: number }
+    | { type: 'KILL_MODAL'; id: number }
+    | { type: 'KILL_ALL'}
+    | { type: 'UNLOCK' }
 
 export type TModalComponent = ReactElement;
 
 export interface IUseModals {
-    get: Record<string, TModal>;
+    get: TModals;
     add: (modalComponent: TModalComponent) => number | false;
     kill: (id: number) => void;
     killAll: any;
@@ -23,89 +36,140 @@ export interface IUseModals {
     closeCurrent: () => void;
 }
 
-export default function useModals(): IUseModals {
-    const [ modals, setModals ] = useState<Record<string, TModal>>({});
-    const [ isChangingState, setIsChangingState ] = useState<boolean>(false);
+function reducer(state: State, action: Action): State {
+    switch (action.type) {
+        case 'ADD_MODAL': {
+            const id = Object.values(state.modals).length;
 
-    function add(modalComponent: TModalComponent) {
-        if (isChangingState) return false;
-        setIsChangingState(true);
+            const props = action.modal.props;
+            const mountDelay = props.mountDelay ?? DEFAULT_DISMOUNT_DELAY;
+            const dismountDelay = props.dismountDelay ?? DEFAULT_DISMOUNT_DELAY;
 
-        const id = Object.values(modals).length;
+            const ModalWithId = React.cloneElement(action.modal, {
+                ...props,
+                id,
+                mountDelay,
+                dismountDelay,
+            });
 
-        const props = modalComponent.props;
-        const mountDelay = props.mountDelay ?? DEFAULT_MOUNT_DELAY;
-        const dismountDelay = props.dismountDelay ?? DEFAULT_DISMOUNT_DELAY;
-        
-        const ModalWithId = React.cloneElement(modalComponent, {
-            ...props,
-            id,
-            mountDelay,
-            dismountDelay,
-        });
-        
-        setModals(prev => {
             return {
-                ...prev,
-                [id.toString()]: {
+                ...state,
+                modals: {
+                  ...state.modals,
+                  [id]: {
                     id,
                     component: ModalWithId,
                     isRemoving: false,
-                    mountDelay: ModalWithId.props.mountDelay,
-                    dismountDelay: ModalWithId.props.dismountDelay
-                }
+                    mountDelay,
+                    dismountDelay,
+                  }
+                },
+                isChangingState: true,
+            };
+        }
+
+        case 'BEGIN_REMOVAL': {
+            if (state.isChangingState) return state;
+
+            const modal = state.modals[action.id];
+            if (!modal) return state;
+
+            return {
+                ...state,
+                modals: {
+                    ...state.modals,
+                    [action.id]: {
+                        ...modal,
+                        isRemoving: true
+                    }
+                },
+                isChangingState: true
+            };
+        }
+
+        case 'KILL_MODAL': {
+            if (!state.modals[action.id]) return state;
+
+            const newModals = { ...state.modals };
+            delete newModals[action.id];
+
+            return {
+                ...state,
+                modals: newModals,
+                isChangingState: false
             }
-        });
+        }
 
-        const delay = ModalWithId.props.mountDelay;
+        case 'KILL_ALL': {
+            return {
+                ...state,
+                modals: {},
+                isChangingState: false
+            }
+        }
 
-        setTimeout(() => {
-            setIsChangingState(false);
-        }, delay);
-        return id;
+        case 'UNLOCK': {
+            return {
+                ...state,
+                isChangingState: false
+            }
+        }
+
+        default: 
+            return state;
     }
+}
 
-    function kill(id: number) {
-        setModals(prev => {
-            const newModals = { ...prev };
-            delete newModals[id];
-            return newModals;
-        });
-        
-        setIsChangingState(false);
-    }
+export default function useModals(): IUseModals {
+    const [ state, dispatch ] = useReducer(reducer, {
+        modals: {},
+        isChangingState: false
+    })
+
+    function add(modalComponent: TModalComponent) {
+        if (state.isChangingState) return false;
+
+        dispatch({ type: 'ADD_MODAL', modal: modalComponent });
+
+        const props = modalComponent.props;
+        const delay = props.mountDelay ?? DEFAULT_MOUNT_DELAY;
     
+        setTimeout(() => {
+            dispatch({ type: 'UNLOCK' });
+        }, delay);
+      
+        return Object.values(state.modals).length;
+    }
+
     function beginRemoval(id: number) {
-        if (isChangingState) return false;
+        if (state.isChangingState) return;
 
-        const delay = modals[id].dismountDelay;
+        const modal = state.modals[id];
+        if (!modal) return;
 
-        setModals(prev => ({...prev, [id]: {
-            ...prev[id],
-            isRemoving: true
-        }}));
+        dispatch({ type: 'BEGIN_REMOVAL', id})
 
         setTimeout(() => {
-            kill(id);
-        }, delay);
-
-        setIsChangingState(true);
+            dispatch({ type: 'KILL_MODAL', id });
+        }, modal.dismountDelay);
     }
 
     function closeCurrent() {
-        const modalArray = Object.values(modals);
+        const modalArray = Object.values(state.modals);
+        if (!modalArray.length) return;
+        
         beginRemoval(modalArray[modalArray.length -1].id);
     }
 
     function killAll() {
-        setModals({});
+        dispatch({ type: 'KILL_ALL' });
     }
 
     return {
-        get: modals,
+        get: state.modals,
         killAll,
         add,
-        kill,
+        kill: (id) => dispatch({ type: 'KILL_MODAL', id }),
         remove: beginRemoval,
         closeCurrent
     }
