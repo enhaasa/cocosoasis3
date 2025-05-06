f6b976ffee13ce64b783a1dc6520b5c851d91b07
Enhasa <enhaasa@pm.me>
2025-05-06 23:22:40 +0200

    Move routing logic to stories file.

--- ladle.config.ts
+++ ladle.config.ts
@@ -1,5 +1,8 @@
+import { resolve } from 'path';
+
 const config = {
-    stories: './src/**/*.stories.@(ts|tsx|js|jsx)', 
-  };
+  stories: './src/**/*.stories.@(ts|tsx)',
+  preview: resolve(__dirname, './ladle/preview.tsx'),
+};
+
 export default config;
-  
--- src/pages/Testaufgabe/Testaufgabe.stories.tsx
+++ src/pages/Testaufgabe/Testaufgabe.stories.tsx
@@ -1,9 +1,19 @@
 import type { Meta } from '@ladle/react';
 import Testaufgabe from './Testaufgabe';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Contexts
 import { UIContextProvider } from '@contexts/UI';
+import { PageContextProvider } from '@contexts/Page';
+import { CMSContextProvider } from '@contexts/CMS';
+import { KiwiContextProvider } from '@contexts/Kiwi';
+import { DiscordContextProvider } from '@contexts/Discord';
 import ModalProvider from '@hooks/modals/ModalProvider';
+
+// Components
+import OffCanvas from '@components/OffCanvas/OffCanvas';
+import ModalManager from '@components/ModalManager/ModalManager';
+import SiteContainer from '@components/SiteContainer/SiteContainer';
 
 export default {
   title: 'Testaufgabe Page',
@@ -11,13 +21,25 @@ export default {
 } satisfies Meta;
 
 export const Default = () => (
-  <BrowserRouter>
-    <UIContextProvider>
-        <ModalProvider>
-            <Routes>
-                <Route path="/" element={<Testaufgabe />} />
-            </Routes>
-        </ModalProvider>
-    </UIContextProvider>
-  </BrowserRouter>
+    <Router>
+        <UIContextProvider>
+            <ModalProvider>
+                <CMSContextProvider>
+                    <KiwiContextProvider>
+                        <DiscordContextProvider>
+                            <PageContextProvider>
+                                <ModalManager />
+                                <OffCanvas />
+                                <SiteContainer>
+                                    <Routes>
+                                        <Route path="/" element={<Testaufgabe />} />
+                                    </Routes>
+                                </SiteContainer>
+                            </PageContextProvider>
+                        </DiscordContextProvider>
+                    </KiwiContextProvider>
+                </CMSContextProvider>
+            </ModalProvider>
+        </UIContextProvider>
+    </Router>
 );
