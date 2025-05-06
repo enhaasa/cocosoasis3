57a5cf49cc650fee7b1c9af3d64b040d20bfcfad
Enhasa <enhaasa@pm.me>
2025-05-06 19:36:58 +0200

    Modal test buttons.

--- src/pages/Testaufgabe/Testaufgabe.tsx
+++ src/pages/Testaufgabe/Testaufgabe.tsx
@@ -9,15 +9,22 @@ import Modal from '@components/Modal/Modal';
 // Context
 import ModalContext from '@hooks/modals/ModalContext';
 
+interface TestModalOptions {
+    mountDelay?: number;
+    dismountDelay?: number;
+}
+
 export default function Testaufgabe() {
     const modals = useContext(ModalContext);
 
-    function handleTestModalClick(mountDelay?: number, dismountDelay?: number) {
+    function handleTestModalClick(options: TestModalOptions) {
+        const { mountDelay, dismountDelay } = options;
+
         const modal = 
         <Modal
             mountDelay={mountDelay}
             dismountDelay={dismountDelay}
-            headline={`mountDelay: ${mountDelay} | dismountDelay: ${dismountDelay}`}
+            headline={`mountDelay: ${mountDelay ?? 'default'} | dismountDelay: ${dismountDelay ?? 'default'}`}
         >
             Hello there.
         </Modal>
@@ -28,8 +35,11 @@ export default function Testaufgabe() {
     return (
         <div className={styles.container}>
             <Title headline='Testaufgabe J.S' size='xxl' />
-
-            <Button name='mountDelay 1000' onClick={handleTestModalClick} />
+        
+            <Button name='All defaults' onClick={() => handleTestModalClick({})} />
+            <Button name='mountDelay 1000' onClick={() => handleTestModalClick({ mountDelay: 1000 })} />
+            <Button name='dismountDelay 1000' onClick={() => handleTestModalClick({ dismountDelay: 1000 })} />
+            <Button name='mountDelay 3000 - dismountDelay 1500' onClick={() => handleTestModalClick({ mountDelay: 3000, dismountDelay: 1500 })} />
         </div>    
     );
 }
